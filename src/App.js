35196ba27803c20f0455308c7544a134c303e47e
Pagorn <pagorn@iwa.fi>
2018-07-12 17:41:40 +0700

    using switch router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 const HomePage = () => <h1>Home</h1>
 const AboutPage = () => <h1>About</h1>
 const PostPage = () => <h1>Post</h1>
 const ProjectPage = () => <h1>Project</h1>
+const NotFoundPage = () => <h1>404: Not found</h1>
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <Switch>
         <Route path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/posts" component={PostPage} />
         <Route path="/projects" component={ProjectPage} />
-      </div>
+        <Route component={NotFoundPage} />
+      </Switch>
     )
   }
 }
