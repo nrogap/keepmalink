9ff43fa956ff900b1f5d82b8b24ed8e010316329
Pagorn <pagorn@iwa.fi>
2018-07-13 11:53:35 +0700

    Remove Switch for pass testing | Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
+
+import 'bulma/css/bulma.css'
 
 const HomePage = () => <h1>Home</h1>
 const AboutPage = () => <h1>About</h1>
@@ -10,14 +12,38 @@ const NotFoundPage = () => <h1>404: Not found</h1>
 class App extends React.Component {
   render() {
     return (
-      <div className="App container">
-        <Switch>
-          <Route path="/" component={HomePage} />
+      <div className="my-app">
+        <nav className="navbar is-light" aria-label="main navigation">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="/">
+                <b>K</b>eepMaLink
+              </a>
+            </div>
+            <div className="navbar-menu">
+              <div className="navbar-end">
+                <NavLink exact to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
+                <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
+                <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
+                <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
+                <a
+                  className="navbar-item"
+                  href="https://github.com/phonbopit"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Star on <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="App container">
+          <Route exact path="/" component={HomePage} />
           <Route path="/about" component={AboutPage} />
-          <Route path="/posts" component={PostPage} />
-          <Route path="/projects" component={ProjectPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+          <Route path="/post" component={PostPage} />
+          <Route path="/project" component={ProjectPage} />
+        </div>
       </div>
     )
   }
