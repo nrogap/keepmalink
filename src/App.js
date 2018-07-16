1fb2677dc444d2e7efc44064f2397f2a9b55ce9d
Pagorn <pagorn@iwa.fi>
2018-07-16 15:57:20 +0700

    Move Header to components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Header from './containers/Header'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomePage from './containers/HomePage'
