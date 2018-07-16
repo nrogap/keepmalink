b0c1ca440d5a6120a66b763e857438d5c2a0d9ae
Pagorn <pagorn@iwa.fi>
2018-07-16 13:30:42 +0700

    Add container style to navbar

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -19,35 +19,42 @@ class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <nav className="navbar is-dark" aria-label="main navigation">
-          <div className="navbar-brand">
-            <a className="navbar-item" href="/">
-              <b>K</b>eepMaLink
-            </a>
-            <div className="navbar-burger burger" role="button" onClick={this.toggleNav}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div className={this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
-            <div className="navbar-end">
-              <NavLink exact to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
-              <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
-              <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
-              <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
-              <a
-                className="navbar-item"
-                href="https://github.com/nrogapp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Star on <i className="fab fa-github"></i>
+        <nav className="navbar is-white is-fixed-top">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="/">
+                <b>K</b>eepMaLink
               </a>
+              <div
+                className="navbar-burger burger"
+                data-target="mainNav"
+                onClick={this.toggleNav}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div
+              id="mainNav"
+              className={this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}
+            >
+              <div className="navbar-end">
+                <NavLink exact to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
+                <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
+                <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
+                <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
+                <a
+                  className="navbar-item"
+                  href="https://github.com/nrogapp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Star on <i className="fab fa-github"></i>
+                </a>
+              </div>
             </div>
           </div>
         </nav>
-
       </div>
     )
   }
