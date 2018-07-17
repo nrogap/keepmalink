82a35ceaeae5555f750e7ee0d4d10809b8b5b0f5
Pagorn <pagorn@iwa.fi>
2018-07-17 16:33:03 +0700

    Add validator on Sign up Page

--- src/containers/SignupPage.js
+++ src/containers/SignupPage.js
@@ -5,37 +5,76 @@ class SignupPage extends React.Component {
     email: '',
     name: '',
     password: '',
-    confirm: '',
-    emailInvalid: null,
-    nameInvalid: null,
-    passwordInvalid: null,
-    confirmPasswordInvalid: null
+    confirmPassword: '',
+    emailMessage: null,
+    nameMessage: null,
+    passwordMessage: null,
+    confirmPasswordMessage: null
   }
 
-  handleEmail = (event) => {
+  handleChange = (event) => {
     const value = event.target.value
+    const id = event.target.id
 
-    const regex = /[\w ]{3,50}/
-    const result = regex.test(value)
-
-    if (result) {
-      this.setState({
-        email: value,
-        emailInvalid: null
-      })
-    } else {
-      this.setState({
-        email: null,
-        emailInvalid: 'length is 3 - 50 characters'
-      })
-    }
+    this.setState({
+      [id]: value
+    })
   }
 
-  handleSubmit = () => {
+  validate() {
+    const {
+      email,
+      name,
+      password,
+      confirmPassword
+    } = this.state
+    const regexEmail = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})/
+    let emailMessage = null
+    if (!email.match(regexEmail)) {
+      emailMessage = 'Please enter a valid email address'
+    }
 
+    const regexName = /[A-z0-9 ]{3,50}$/
+    let nameMessage = null
+    if (!name.match(regexName)) {
+      nameMessage = 'Can only contain letters and numbers'
+    }
+
+    let passwordMessage = null
+    if (password.length < 8) {
+      passwordMessage = 'Use 8 characters or mores'
+    }
+
+    let confirmPasswordMessage = null
+    if (confirmPassword !== password) {
+      confirmPasswordMessage = 'does not match the password'
+    }
+
+    this.setState({
+      emailMessage,
+      nameMessage,
+      passwordMessage,
+      confirmPasswordMessage
+    }, () => {
+      console.log('state: ', this.state)
+    })
+  }
+
+  // length is 3-50 characters
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    this.validate()
   }
 
   render() {
+    const {
+      emailMessage,
+      nameMessage,
+      passwordMessage,
+      confirmPasswordMessage
+    } = this.state
+
     return (
       <div className="SignupPage">
         <form onSubmit={this.handleSubmit}>
@@ -43,66 +82,81 @@ class SignupPage extends React.Component {
             <label className="label">Email</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                className="input is-danger"
-                id="inputEmail"
+                className={`input ${emailMessage ? 'is-danger' : ''}`}
+                id="email"
                 type="email"
                 autoComplete="email"
-                onChange={this.handleEmail} />
+                onChange={this.handleChange} />
               <span className="icon is-small is-left">
                 <i className="fas fa-envelope"></i>
               </span>
             </div>
-            <p className="help is-danger">This email is invalid</p>
+            {emailMessage
+              ? <p className="help is-danger">{emailMessage}</p>
+              : null
+            }
           </div>
 
           <div className="field">
             <label className="label">Name</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                className="input is-success"
-                id="inputName"
+                className={`input ${nameMessage ? 'is-danger' : ''}`}
+                id="name"
                 type="text"
-                autoComplete="name" />
+                autoComplete="name"
+                onChange={this.handleChange} />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
               </span>
             </div>
-            <p className="help is-success">This name is available</p>
+            {nameMessage
+              ? <p className="help is-danger">{nameMessage}</p>
+              : null
+            }
           </div>
 
           <div className="field">
             <label className="label">Password</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                className="input is-success"
-                id="inputPassword"
+                className={`input ${passwordMessage ? 'is-danger' : ''}`}
+                id="password"
                 type="password"
-                autoComplete="new-password" />
+                autoComplete="new-password"
+                onChange={this.handleChange} />
               <span className="icon is-small is-left">
                 <i className="fas fa-key"></i>
               </span>
             </div>
-            <p className="help is-success">This password is valid</p>
+            {passwordMessage
+              ? <p className="help is-danger">{passwordMessage}</p>
+              : null
+            }
           </div>
 
           <div className="field">
             <label className="label">Confirm Password</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                className="input is-success"
-                id="inputConfirmPassword"
+                className={`input ${confirmPasswordMessage ? 'is-danger' : ''}`}
+                id="confirmPassword"
                 type="password"
-                autoComplete="new-password" />
+                autoComplete="new-password"
+                onChange={this.handleChange} />
               <span className="icon is-small is-left">
                 <i className="fas fa-key"></i>
               </span>
             </div>
-            <p className="help is-success">This confirm password is invalid</p>
+            {confirmPasswordMessage
+              ? <p className="help is-danger">{confirmPasswordMessage}</p>
+              : null
+            }
           </div>
 
           <div className="field is-grouped">
             <div className="control">
-              <button className="button is-link">Submit</button>
+              <button className="button is-link" type="submit">Submit</button>
             </div>
           </div>
         </form>
