bd38073a0553f3b7b178ac58f8adeb6f38dc3f42
nrogap <nrogap.p@gmail.com>
2018-07-19 13:31:42 +0700

    Remove firebaseConfig

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
-import firebaseConfig from '../firebase/firebaseConfig2'
-
 class HomePage extends React.Component {
   render() {
     console.log(process.env)
-    console.log('firebaseConfig: ', firebaseConfig)
     return (<h1>Home</h1>)
   }
 }
