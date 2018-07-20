0d9e593cc26ff848b861feccd0b89f5c0c475ec8
nrogap <nrogap.p@gmail.com>
2018-07-20 13:43:20 +0700

    Add sign up link on SignInPage

--- src/containers/SigninPage.js
+++ src/containers/SigninPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import * as routes from '../constants/routes'
 
 class SignInPage extends React.Component {
   render() {
-    return (<div>SignInPage</div>)
+    return (
+      <div>
+        SignInPage
+        <SignUpLink />
+      </div>
+    )
   }
 }
+
+const SignUpLink = () =>
+  <p>
+    Don't have an account?
+    {' '}
+    <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
 
 export default SignInPage
